d39a1129c575f6b9499ecb207478fa0b14897e16
foyama <felipe.oyama@mercadolivre.com>
2022-09-15 11:15:05 -0300

    remove structs

--- cmd/main.go
+++ cmd/main.go
@@ -6,19 +6,6 @@ import (
 	"github.com/tcalazans/go-api/internal/service/taylorswift_service"
 )
 
-type apiResponse struct {
-	// padrao snakecase em go na parte do Json
-	SwiftMusicData *swiftMusicData `json:"swift_music_data,omitempty"`
-	Partial        bool            `json:"partial"`
-}
-
-type swiftMusicData struct {
-	// omitempty vai omitir caso o objeto esteja vazio
-	Quote string `json:"quote,omitempty"`
-	Song  string `json:"song,omitempty"`
-	Album string `json:"album,omitempty"`
-}
-
 func main() {
 	taylorSwiftService := taylorswift_service.NewTaylorSwiftService()
 	taylorSwiftController := taylorswift_controller.NewTaylorSwiftController(taylorSwiftService)
